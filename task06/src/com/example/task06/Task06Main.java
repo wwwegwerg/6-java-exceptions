346f09e1ff17a2199958f0c87f566eaccd507109
Daniil <xmegatech89@gmail.com>
2025-09-13 14:01:52 +0500

    complete tasks 1-7

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -2,15 +2,15 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         new Task06Main().printMethodName();
-         */
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println(e.getStackTrace()[1].getMethodName());
+        }
     }
 
 }
